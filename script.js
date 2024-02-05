33492c80147eea29407e713c266bcd4091b91598
Hana Pavlova <hanapavlova7@gmail.com>
2024-02-05 13:55:56 +0100

    priklady 1 a 2

---
+++ script.js
@@ -0,0 +1,35 @@
+/*
+Rozdělení peněz mezi lidi:
+Máte určitou částku peněz (např. 11237 Kč).
+Rozdělte tuto částku rovnoměrně mezi určitý počet lidí (např. 5).
+Vypište každému, kolik dostal peněz.
+a kolik zbývá v zůstatku
+*/
+const castka = 11237;
+const castkaNaOsobu = Math.floor (11237/5);
+const zbytek = castka % 5;
+
+document.body.innerHTML += "Každý dostane " + castkaNaOsobu + " Kč, ve fondu zbyde " + zbytek + " Kč.";
+
+/*
+2.Zaokrouhlení desetinného čísla:
+Máte desetinné číslo (např. 8.75).
+Zaokrouhlete toto číslo na celé číslo pomocí Math.round a vypište výsledek.
+*/
+const cislo = 8.75;
+const vysledek = Math.round(cislo);
+
+document.body.innerHTML += "<p>Výsledek příkladu je " + vysledek + ".</p>";
+
+/*
+3. Výpočet daně z nákupu s DPH:Máte částku za zboží před DPH (např. 325 Kč).
+Přidejte 21 % DPH k ceně zboží.
+Vypište cenu zboží, DPH a celkovou cenu do HTML.
+*/
+
+/*
+4. Výpočet daně z příjmu s odečtem paušálu:Máte příjem z podnikání (např. 15000 Kč).
+Odečtěte 60 % z příjmu jako paušál.
+Ze zbytku zaplaťte 15 % daně.
+Zaokrouhlete výsledek dolů a vypište daň do HTML.
+*/
