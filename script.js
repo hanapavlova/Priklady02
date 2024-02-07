c8459dab0cd8e4a63f78921489a8bbb345dd0d56
Hana Pavlova <hanapavlova7@gmail.com>
2024-02-07 10:21:58 +0100

    zadani prikladu 5-7

--- script.js
+++ script.js
@@ -45,3 +45,25 @@ const zaklad = prijem-pausal;
 const dan = Math.floor(zaklad*0.15);
 
 document.body.innerHTML += "<p>Daň z příjmu je " + dan + " Kč (po zaokrouhlení).</p>"
+
+/*
+5. Kalkulace nákladů na pronájem kol. Cena bez slevy je 257,-
+Na půjčení kol platí následující slevový systém:
+Při zapůjčení kola na 3 dny získáte 10% slevu.
+Při zapůjčení kola na 5 dní získáte slevu 15 %.
+Při zapůjčení kola na 7 a více dní je poskytována sleva 20 %.
+Napište program, který vypočítá náklady na zapůjčení kola s přihlédnutím ke zvolené době pronájmu a odpovídající slevě.*/
+
+/*
+6. Výpočet nákladů na oběd v restauraci
+Restaurace má speciální nabídku na pracovní oběd: oběd stojí 500 Kč. Pro stálé zákazníky je však sleva 20 %. Napište program, který spočítá celkové náklady na jídlo, pokud by bylo 15 stálých zákazníků a 7 stálých zákazníků.
+*/
+
+/*
+7. Výpočet ceny jízdenky s přihlédnutím ke slevě
+V kině probíhá akce, která poskytuje různé slevy na vstupenky v závislosti na věkové kategorii návštěvníků. Cena běžné jízdenky je 300 kč.
+Důchodci a děti do 16 let mají slevu 25 %. Do kina přišlo 10 důchodců a 5 dětí do 16 let.
+Studenti mají 50% slevu. Do kina přišlo 7 studentů.
+Děti do 7 let navštěvují kino zdarma. Do kina přišly 2 děti této věkové kategorie.
+Je nutné sepsat program pro výpočet celkových nákladů na vstupenky s přihlédnutím ke všem poskytnutým slevám a celkový počet návštěvníků.
+*/
